de7fc35cc8c4bd5eb85497a28b0d77f9b5efc5e9
unknown <rikenmaharjan4@gmail.com>
2025-12-02 14:15:39 +0545

    first commit

---
+++ main/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from . import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.home, name='home'), 
+    path('price/', views.price, name='price'),
+    path('price_detail/', views.price_detail, name='price_detail'),
+    path('register_agent/', views.register_agent, name='register_agent'),
+    path('company_formation/', views.company_formation, name='company_formation'),
+    path('ein_registration/', views.ein_registration, name='ein_registration'),
+    path('annual_report_filing/', views.annual_report_filing, name='annual_report_filing'),
+    path('expert_taxes/', views.expert_taxes, name='expert_taxes'),
+    path('file_your_own_taxes/', views.file_your_own_taxes, name='file_your_own_taxes'),
+    path('refund_advance/', views.refund_advance, name='refund_advance'),
+    path('flex_advance/', views.flex_advance, name='flex_advance'),
+    path('account/', views.account, name='account'),
+    path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
+    path('blog_detail/', views.blog_detail, name='blog_detail'),
+    path('team/', views.team, name='team'),
+    path('faq/', views.faq, name='faq'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('term_of_services/', views.term_of_services, name='term_of_services'),
+
+
+
+]
---
+++ main/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html') 
+
+def price(request):
+    return render(request, 'price.html') 
+
+def price_detail(request):
+    return render(request, 'price_detail.html') 
+
+def register_agent(request):
+    return render(request, 'register_agent.html') 
+
+def company_formation(request):
+    return render(request, 'company_formation.html') 
+
+def ein_registration(request):
+    return render(request, 'ein_registration.html') 
+
+def annual_report_filing(request):
+    return render(request, 'annual_report_filing.html') 
+
+def expert_taxes(request):
+    return render(request, 'expert_taxes.html') 
+
+def file_your_own_taxes(request):
+    return render(request, 'file_your_own_taxes.html') 
+
+def refund_advance(request):
+    return render(request, 'refund_advance.html') 
+
+def flex_advance(request):
+    return render(request, 'flex_advance.html') 
+
+def account(request):
+    return render(request, 'account.html') 
+
+def about(request):
+    return render(request, 'nav info/about.html') 
+
+def blog(request):
+    return render(request, 'nav info/blog.html') 
+
+
+def blog_detail(request):
+    return render(request, 'blog_detail.html') 
+
+def team(request):
+    return render(request, 'nav info/team.html') 
+
+
+def faq(request):
+    return render(request, 'nav info/faq.html') 
+
+
+def privacy_policy(request):
+    return render(request, 'nav info/privacy_policy.html') 
+
+
+def term_of_services(request):
+    return render(request, 'nav info/term_of_services.html') 
+
